8c77e59dd348edddc2203cec166a594089706fa5
YuriyB <paganism@mail.ru>
2018-09-12 09:36:34 +0300

    some modifications

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, flash, url_for, current_app
+from flask import Flask, render_template, redirect, flash, url_for, current_app, request
 from app.models import Ads
 from app import db
 from app.main import bp
@@ -14,7 +14,16 @@ def before_request():
 @bp.route('/', methods=['GET', 'POST'])
 # @app.route('/index', methods=['GET', 'POST'])
 def ads_list():
-    return render_template('ads_list.html', ads=[{
+    oblast_district = request.args.get('oblast_district')
+    new_building = request.args.get('new_building')
+    min_price = request.args.get('min_price')
+    max_price = request.args.get('max_price')
+    print(oblast_district, new_building, min_price, max_price)
+    if not min_price:
+        print('HVG')
+    adss = Ads.query.filter_by(oblast_district=oblast_district, construction_year='2010')
+    print(adss)
+    ads = [{
             "settlement": "Череповец",
             "under_construction": False,
             "description": '''Квартира в отличном состоянии. Заезжай и живи!''',
@@ -27,4 +36,5 @@ def ads_list():
             "rooms_number": 2,
             "premise_area": 43.0,
         }]*10
-    )
+    return render_template('ads_list.html', ads=ads)
+
